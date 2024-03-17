f57ef25fbfafc1b1979eafef911e2f1d59988493
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-03-18 02:00:03 +0600

    day3

--- src/Component/Allprodect.jsx
+++ src/Component/Allprodect.jsx
@@ -1,74 +1,72 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Button, Space, Table, Tag } from 'antd';
 // import Allprodect from './Allprodect';
-const columns = [
-  {
-    title: 'Product Name',
-    dataIndex: 'name',
-    key: 'name',
-   
-  },
-  {
-    title: 'Image',
-    dataIndex: 'Image',
-    key: 'Image',
-    render: (_, record) => (
-       <img style={{ width:'100px' }} src='https://istockbd.com/cdn/shop/products/iPhone-14-Pro-Deep-Purple-Price-in-Bangladesh.png?v=1663330585&width=600' alt='Iphone'></img>
-      ),
-    
-  },
-  {
-    title: 'Store Name',
-    dataIndex: 'Store',
-    key: 'Store',
-  },
-  
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-       <Button type='primary'>Eidt</Button>
-        <Button type='primary' danger>Delete</Button>
-      </Space>
-    ),
-  },
-];
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+
+
 const Allprodect = () => {
+  const [procdatData,SetprodactData]=useState([]);
     useEffect(()=>{
  async function Allprodects(){
   let data= await  axios.get('http://localhost:3000/api/v1/producat/getproducat')
-  console.log(data, 'data');
+  SetprodactData(data.data);
   }
   Allprodects()
     },[])
+
+ const handledelate =(id)=>{
+  console.log('del', id);
+ }
+
+    const columns = [
+
+      {
+        title: 'Serial No',
+        dataIndex: 'index',
+        key: 'index',
+        render: (_, record,index) => 
+       {return index +1 }
+         
+      },
+      {
+        title: 'Product Name',
+        dataIndex: 'name',
+        key: 'name',
+       
+      },
+      {
+        title: 'Image',
+        dataIndex: 'Image',
+        key: 'Image',
+        render: (_, record) => (
+           <img style={{ width:'100px' }} src='https://istockbd.com/cdn/shop/products/iPhone-14-Pro-Deep-Purple-Price-in-Bangladesh.png?v=1663330585&width=600' alt='Iphone'></img>
+          ),
+        
+      },
+      
+      {
+        title: 'Store Name',
+        dataIndex: 'Storename',
+        key: 'Storename',
+        render: (_, record) => (
+          <p>{record.store.storename}</p>
+         ),
+      },
+      
+      {
+        title: 'Action',
+        key: 'action',
+        render: (_, record) => (
+          <Space size="middle">
+           <Button type='primary'>Eidt</Button>
+            <Button onClick={()=>handledelate(record. _id)} type='primary' danger>Delete </Button>
+          </Space>
+        ),
+      },
+    ];
   return (
     <div>
-        <Table columns={columns} dataSource={data} />
+        <Table columns={columns} dataSource={procdatData} />
     </div>
   )
 }
