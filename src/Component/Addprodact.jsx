eaeda37ebaf24757e866216517d91897bb2bbc8e
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-03-20 03:58:22 +0600

    day4

--- src/Component/Addprodact.jsx
+++ src/Component/Addprodact.jsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react'
-import { Input } from 'antd';
+import React, { useEffect, useState } from 'react'
+import { Button, Input } from 'antd';
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState } from 'draft-js';
+import { EditorState,convertToRaw } from 'draft-js';
 import { Select, Tag } from 'antd';
-const options = [
-  {
-    value: 'gold',
-  },
-  {
-    value: 'lime',
-  },
-  {
-    value: 'green',
-  },
-  {
-    value: 'cyan',
-  },
-];
+import axios from 'axios';
+import draftToHtml from 'draftjs-to-html';
 
 const tagRender = (props) => {
   const { label, value, closable, onClose } = props;
@@ -41,18 +29,52 @@ const tagRender = (props) => {
 };
 
 const Addprodact = () => {
+
+  const [store, setStore] = useState([]);
+  const [data, setData] = useState([]);
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [producat, Setprodactname] = useState('');
+ const [storeName, SetstoreName]=useState('')
+
 
   const onEditorStateChange = (editorState) => {
     setEditorState(editorState);
   };
+
+  useEffect(() => {
+    async function allstoredata() {
+      let arr = []
+      const data = await axios.get('http://localhost:3000/api/v1/merchant/allstore');
+      setStore(data.data);
+
+      data.data.map((Item) => {
+        arr.push({
+          value: Item._id,
+          label: Item.storename
+
+        })
+      })
+      setData(arr)
+    }
+    allstoredata()
+  }, [])
+
+
+
+  const handleProducatUpload = () => {
+
+    console.log(producat);
+    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+    console.log(storeName);
+  }
+
   return (
     <div>
-    <h1> Product Name:</h1>
-    <Input placeholder="Product Name" />
+      <h1 onChange={(e) => Setprodactname(e)}> Product Name:</h1>
+      <Input placeholder="Product Name" />
 
-    <h1> Product Description:</h1>
-    <Editor
+      <h1> Product Description:</h1>
+      <Editor
         editorState={editorState}
         wrapperClassName="demo-wrapper"
         editorClassName="demo-editor"
@@ -61,25 +83,29 @@ const Addprodact = () => {
 
       <h1>Product variant</h1>
       <Select
-    mode="multiple"
-    tagRender={tagRender}
+        mode="multiple"
+        tagRender={tagRender}
 
-    style={{
-      width: '100%',
-    }}
-    options={options}
-  />
+        style={{
+          width: '100%',
+        }}
+      
+      />
 
-  <h1>Producat Store Name:</h1>
-  <Select
-    mode="single"
-    tagRender={tagRender}
+      <h1>Producat Store Name:</h1>
+      <Select
+      onChange={(e)=> SetstoreName (e)}
+        mode="single"
+        tagRender={tagRender}
 
-    style={{
-      width: '100%',
-    }}
-    options={options}
-  />
+        style={{
+          width: '100%',
+        }}
+        options={data}
+      />
+      <br />
+      <br />
+      <Button onClick={handleProducatUpload} type='primary'>Producat Upload</Button>
     </div>
   )
 }
--- src/Component/Allprodect.jsx
+++ src/Component/Allprodect.jsx
@@ -1,72 +1,89 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Button, Space, Table, Tag } from 'antd';
-// import Allprodect from './Allprodect';
+import { Button, Space, Table,message, Popconfirm } from 'antd';
+
 
 
 const Allprodect = () => {
-  const [procdatData,SetprodactData]=useState([]);
-    useEffect(()=>{
- async function Allprodects(){
-  let data= await  axios.get('http://localhost:3000/api/v1/producat/getproducat')
-  SetprodactData(data.data);
+  const [open, setOpen] = useState(false);
+  const [procdatData, SetprodactData] = useState([]);
+
+  const confirm = () => {
+    setOpen(false);
+    message.success('Delete Suncessfully Done');
+  };
+
+
+
+
+
+
+  useEffect(() => {
+    async function Allprodects() {
+      let data = await axios.get('http://localhost:3000/api/v1/producat/getproducat')
+      SetprodactData(data.data);
+    }
+    Allprodects()
+  }, [procdatData])
+
+  const handledelate = async (id) => {
+    let data = await axios.post('http://localhost:3000/api/v1/producat/delateProdact', {
+      id: id
+    })
+    console.log(data);
+    confirm();
   }
-  Allprodects()
-    },[])
 
- const handledelate =(id)=>{
-  console.log('del', id);
- }
+  const columns = [
 
-    const columns = [
+    {
+      title: 'Serial No',
+      dataIndex: 'index',
+      key: 'index',
+      render: (_, record, index) => { return index + 1 }
 
-      {
-        title: 'Serial No',
-        dataIndex: 'index',
-        key: 'index',
-        render: (_, record,index) => 
-       {return index +1 }
-         
-      },
-      {
-        title: 'Product Name',
-        dataIndex: 'name',
-        key: 'name',
-       
-      },
-      {
-        title: 'Image',
-        dataIndex: 'Image',
-        key: 'Image',
-        render: (_, record) => (
-           <img style={{ width:'100px' }} src='https://istockbd.com/cdn/shop/products/iPhone-14-Pro-Deep-Purple-Price-in-Bangladesh.png?v=1663330585&width=600' alt='Iphone'></img>
-          ),
-        
-      },
-      
-      {
-        title: 'Store Name',
-        dataIndex: 'Storename',
-        key: 'Storename',
-        render: (_, record) => (
-          <p>{record.store.storename}</p>
-         ),
-      },
-      
-      {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-          <Space size="middle">
-           <Button type='primary'>Eidt</Button>
-            <Button onClick={()=>handledelate(record. _id)} type='primary' danger>Delete </Button>
-          </Space>
-        ),
-      },
-    ];
+    },
+    {
+      title: 'Product Name',
+      dataIndex: 'name',
+      key: 'name',
+
+    },
+    {
+      title: 'Image',
+      dataIndex: 'Image',
+      key: 'Image',
+      render: (_, record) => (
+        <img style={{ width: '100px' }} src='https://istockbd.com/cdn/shop/products/iPhone-14-Pro-Deep-Purple-Price-in-Bangladesh.png?v=1663330585&width=600' alt='Iphone'></img>
+      ),
+
+    },
+
+    {
+      title: 'Store Name',
+      dataIndex: 'Storename',
+      key: 'Storename',
+      render: (_, record) => (
+        <p>{record.store.storename}</p>
+      ),
+    },
+
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <Button type='primary'>Eidt</Button>
+          <Popconfirm open={open} onConfirm={confirm}>
+            <Button onClick={() => handledelate(record._id)} type='primary' danger>Delete </Button>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
   return (
     <div>
-        <Table columns={columns} dataSource={procdatData} />
+      <Table columns={columns} dataSource={procdatData} />
     </div>
   )
 }
